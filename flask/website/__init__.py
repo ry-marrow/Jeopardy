d7058c8a5153fb43335f6a1a46f2608faf667f29
Surya Prakash <surya96t@gmail.com>
2023-10-08 14:53:25 -0400

    update

--- flask/website/__init__.py
+++ flask/website/__init__.py
@@ -21,6 +21,10 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     
+    from .models import User, Note
+    
+    with app.app_context():
+        db.create_all()
 
     
     # create_database(app)
