b1d8c148f3df21a08f03172e203cd0e3ae69b839
Surya Prakash <surya96t@gmail.com>
2023-10-17 16:04:38 -0400

    added gameboard

--- flask/website/static/index.js
+++ flask/website/static/index.js
@@ -1,8 +1,73 @@
-function deleteNote(noteId){
-    fetch('/delete-note', {
-        method: 'POST',
-        body: JSON.stringify({noteId:noteId})
-    }).then((_res) => {
-        window.location.href = "/"  //refresh the page
-    });
+// function deleteNote(noteId){
+//     fetch('/delete-note', {
+//         method: 'POST',
+//         body: JSON.stringify({noteId:noteId})
+//     }).then((_res) => {
+//         window.location.href = "/"  //refresh the page
+//     });
+// }
+
+// Define a variable to track the dollar total
+let dollarTotal = 0;
+
+// Function to update the dollar total and display it
+function updateDollarTotal(increment) {
+    dollarTotal += increment;
+    // Update the displayed total in the HTML
+    const totalDisplay = document.getElementById('dollar-total');
+    totalDisplay.textContent = dollarTotal; // Update the content
+
+    // Add the dollar sign and formatting
+    const formattedTotal = dollarTotal;
+    totalDisplay.textContent = formattedTotal;
 }
+
+// Wait for the DOM to load
+document.addEventListener('DOMContentLoaded', function() {
+    // Get references to the popup and iframe
+    const questionPopup = document.getElementById('question-popup');
+    const questionIframe = document.getElementById('question-iframe');
+    const cells = document.querySelectorAll('.question-trigger');
+
+    let point_value = 0; // Initialize the point_value variable
+
+    // Add a click event listener to each gameboard cell
+    cells.forEach(function(cell) {
+        cell.addEventListener('click', function(event) {
+            event.preventDefault();
+
+            // Get the category_id and point_value from data attributes
+            const category_id = this.getAttribute('data-category-id');
+            point_value = this.getAttribute('data-point-value'); // Update point_value
+
+            // Set the iframe source to the question route
+            questionIframe.src = `/question/${category_id}/${point_value}/options`;
+
+            // Show the question popup
+            questionPopup.style.display = 'block';
+        });
+    });
+
+    // Function to receive messages from the iframe
+    function receiveMessage(event) {
+        if (event.data && event.data.answer) {
+            const answer = event.data.answer;
+            const correctAnswer = "A. Python"; // Update with the actual correct answer
+
+            if (answer === correctAnswer) {
+                // Increment the dollar total by the question's point value
+                updateDollarTotal(parseInt(point_value));
+                alert("Correct answer! Your total is now $" + dollarTotal);
+            } else {
+                // Handle incorrect answers here (if needed)
+                alert("Incorrect answer. Try again.");
+            }
+
+            // Close the popup
+            questionPopup.style.display = 'none';
+        }
+    }
+
+    // Listen for messages from the iframe
+    window.addEventListener('message', receiveMessage, false);
+});
--- flask/website/views.py
+++ flask/website/views.py
@@ -18,26 +18,13 @@ views = Blueprint('views', __name__) # defining blueprint
 @views.route('/', methods=['GET', 'POST'])
 @login_required # cannot get to home page unless you are logged in
 def home():
-    if request.method == 'POST':
-        note = request.form.get('note')
-        
-        if len(note) < 1:
-            flash('Note is too short!', category='error')
-        else:
-            new_note = Note(data=note, user_id=current_user.id)
-            db.session.add(new_note)
-            db.session.commit()
-            flash('Note added!', category='success')
     return render_template("home.html", user=current_user)
 
-@views.route('/delete-note', methods=['POST'])
-def delete_note():
-    note = json.loads(request.data)
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
-    if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
-            
-    return jsonify({})
+@views.route('/jeopardy', methods=['GET', 'POST'])
+@login_required
+def jeopardy():
+    return render_template("jeopardy.html", user=current_user)
+
+@views.route('/question/<int:category_id>/<int:point_value>/options')
+def question_options(category_id, point_value):
+    return render_template('question.html')
