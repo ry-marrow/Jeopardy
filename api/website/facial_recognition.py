06218bfbf54e132e2ecf9778003144c155e99337
Ryan Merow <recon.7181@comcast.net>
2023-12-04 01:31:00 -0500

    adjusted image navi in file

--- api/website/facial_recognition.py
+++ api/website/facial_recognition.py
@@ -12,11 +12,11 @@ facial_recognition= Blueprint('face', __name__)
 camera = cv2.VideoCapture(0)
 
 # Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("website\face_images\obama.jpg")
+obama_image = face_recognition.load_image_file("website/face_images/obama.jpg")
 obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-biden_image = face_recognition.load_image_file("website\face_images\biden.jpg")
+biden_image = face_recognition.load_image_file("website/face_images/biden.jpg")
 biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
 
 # Create arrays of known face encodings and their names
