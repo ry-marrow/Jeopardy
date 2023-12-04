0da12100bc62426c333dc1149b95b11606119575
Ryan Merow <recon.7181@comcast.net>
2023-12-04 02:08:21 -0500

    adjusted inits model view

--- api/website/__init__.py
+++ api/website/__init__.py
@@ -29,7 +29,7 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
     app.register_blueprint(facial_recognition, url_prefix='/')
     
-    from models import User, questions_answers, game_data
+    from .models import User, questions_answers, game_data
     
     with app.app_context():
         db.create_all()
