b7e37eb06a97089914f75464571025f0eb1bdde2
Ryan Merow <recon.7181@comcast.net>
2023-12-04 02:03:18 -0500

    removed initial face id setup with joe and obama

--- api/website/facial_recognition.py
+++ api/website/facial_recognition.py
@@ -12,22 +12,22 @@ facial_recognition= Blueprint('face', __name__)
 camera = cv2.VideoCapture(0)
 
 # Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file(r"face_images/obama.jpg")
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+# obama_image = face_recognition.load_image_file(r"face_images/obama.jpg")
+# obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-biden_image = face_recognition.load_image_file(r"face_images/biden.jpg")
-biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+# biden_image = face_recognition.load_image_file(r"face_images/biden.jpg")
+# biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
 
 # Create arrays of known face encodings and their names
-known_face_encodings = [
-    obama_face_encoding,
-    biden_face_encoding
-]
-known_face_names = [
-    "Barack Obama",
-    "Joe Biden"
-]
+# known_face_encodings = [
+#     obama_face_encoding,
+#     biden_face_encoding
+# ]
+# known_face_names = [
+#     "Barack Obama",
+#     "Joe Biden"
+# ]
 
 # Initialize some variables
 face_locations = []
